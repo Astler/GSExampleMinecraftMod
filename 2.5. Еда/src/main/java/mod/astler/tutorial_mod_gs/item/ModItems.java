2301d9b95d967218f361f56f9f40a06fd43aa244
Astler <vladyclaw@gmail.com>
2020-06-08 23:36:30 +0300

    Обновил гайд о еде

--- 2.5. Еда/src/main/java/mod/astler/tutorial_mod_gs/item/ModItems.java
+++ 2.5. Еда/src/main/java/mod/astler/tutorial_mod_gs/item/ModItems.java
@@ -1,6 +1,7 @@
 package mod.astler.tutorial_mod_gs.item;
 
 import mod.astler.tutorial_mod_gs.TutorialGSMod;
+import net.minecraft.item.Foods;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,7 +10,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, TutorialGSMod.MODID);
 
-    public static final RegistryObject<Item> SAUSAGE = ITEMS.register("sausage", () -> new Item(new Item.Properties().food(ModFood.SAUSAGE).group(ModItemGroups.MOD_ITEMS_ITEM_GROUP)));
-    public static final RegistryObject<Item> ENCHANTED_SAUSAGE = ITEMS.register("enchanted_sausage", () -> new EnchantedSausage(new Item.Properties().food(ModFood.ENCHANTED_SAUSAGE).group(ModItemGroups.MOD_ITEMS_ITEM_GROUP)));
+    public static final RegistryObject<Item> SAUSAGE = ITEMS.register("sausage", () -> new Item(new Item.Properties().food(ModFoods.SAUSAGE).group(ModItemGroups.MOD_ITEMS_ITEM_GROUP)));
+    public static final RegistryObject<Item> ENCHANTED_SAUSAGE = ITEMS.register("enchanted_sausage", () -> new EnchantedSausage(new Item.Properties().food(ModFoods.ENCHANTED_SAUSAGE).group(ModItemGroups.MOD_ITEMS_ITEM_GROUP)));
 }
 
--- 3. Создаём блок/src/main/java/mod/astler/tutorial_mod_gs/item/ModItemGroups.java
+++
@@ -1,7 +0,0 @@
-package mod.astler.tutorial_mod_gs.item;
-
-import net.minecraft.item.ItemGroup;
-
-public class ModItemGroups {
-    public static final ItemGroup MOD_ITEMS_ITEM_GROUP = new ModItemGroup("tutorial_mod_gs_items", ModItems.STONE_STICK);
-}
